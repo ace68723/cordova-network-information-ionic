122d5c246d64488f43055e105927161e6cf0e9ee
ace68723 <yichenghuang@yichengs-Mac-mini-2.local>
2014-12-24 14:02:06 -0500

    network module

---
+++ www/common/network.js
@@ -0,0 +1,39 @@
+'use strict'
+
+var network = angular.module('NetworkInfo',[]);
+	
+	network.factory('networkService',function  ($cordovaNetwork) {
+		this.network = function(){
+	      if($cordovaNetwork.isOnline()) {
+	       return this.connectionType = 'Online';
+	      }
+	      else if ($cordovaNetwork.isOffline()) {
+	         return this.connectionType = 'Offline';
+	      }
+	      else {
+	        return this.errorMsg = 'Error getting isOffline / isOnline methods';
+	      }
+	  	}
+	  	return {
+	  			network: this.network
+	  			}
+	})
+
+	.run(function ($rootScope, $ionicPlatform, $cordovaNetwork) {
+
+    $ionicPlatform.ready(function () {
+      
+       $cordovaNetwork.watchOffline();
+       $cordovaNetwork.watchOnline();
+
+
+      $rootScope.$on("networkOffline", function () {
+        alert("iPad is now Offline!");
+      });
+
+
+      $rootScope.$on("networkOnline", function () {
+        alert("iPad is Online!");
+      });
+    })
+});
